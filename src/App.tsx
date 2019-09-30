79430af7d8bf5ff3c9b0e4a3284f9fdb4ed9adf5
Olimpio Pimenta <olimpiodx@gmail.com>
2019-09-29 21:34:42 -0300

    exemplo simples com useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Callbac from './callback/calback';
+import AdicionandoElementos from './useEffect/AdicionandoElementos';
 
 function App() {
   const [texto, setTexto] = useState('');
+
+  const [pessoas, setPessoas] = useState(['pessoa ', 'pessoa 1 ']);
+  useEffect(() => {}, [pessoas]);
 
   function Change(event: React.ChangeEvent<HTMLInputElement>) {
     setTexto(event.target.value);
     console.log('event.target.value', event.target.value);
   }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +22,16 @@ function App() {
         <h3>Exemplo simples usando callback</h3>
         <Callbac change={() => Change} />
         <p> seu texto => {texto}</p>
+        <h3>Lista de pessoas com useEffect</h3>
+        <ul>
+          <li>{pessoas}</li>
+        </ul>
+
+        <button
+          onClick={() => setPessoas([...pessoas, `produto-${pessoas.length} `])}
+        >
+          Add
+        </button>
       </header>
     </div>
   );
